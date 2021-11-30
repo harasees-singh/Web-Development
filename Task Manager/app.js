742739836ae43e6b4b17a34e5e57f722448e22a2
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-11-30 15:59:38 +0530

    completed

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -17,8 +17,8 @@ app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
-
+// const port = process.env.PORT || 5000;
+const port = 5000
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
