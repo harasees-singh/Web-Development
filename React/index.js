e7bb2efc468dd90e904c283536136bac2e3630e5
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-01-31 19:47:09 +0530

    ✔️

---
+++ React/index.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<h1>Hello, pephole!</h1>, document.getElementById("root"))
--- Task Manager/app.js
+++ Task Manager/app.js
@@ -20,14 +20,14 @@ app.use(errorHandlerMiddleware);
 // const port = process.env.PORT || 5000;
 const port = 5000
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
